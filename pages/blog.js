524a67e684b4b135274a9926ad2a5538c155b7ce
ManjiCoder <manjicoder@gmail.com>
2023-06-05 15:03:04 +0530

    Tutorial #15 ✔ Creating Blog and Blogpost page

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,37 @@
 import React from "react";
-
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 const Blog = () => {
-  return <div>This is Blog page</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className="blogItem">
+          <h3>
+            <Link href={"/blogpost/learn-Javascript"}>
+              How to learn Javascript in 2023
+            </Link>
+          </h3>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>
+            <Link href={"/blogpost/learn-Python"}>
+              How to learn Python in 2023
+            </Link>
+          </h3>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>
+            <Link href={"/blogpost/learn-Android"}>
+              How to learn Android in 2023
+            </Link>
+          </h3>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Blog;
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,12 +1,26 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useRouter } from "next/router";
 import React from "react";
+import styles from "../../styles/BlogPost.module.css";
 
 const slug = () => {
   const router = useRouter();
   console.log(router.query.slug);
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+          provident nulla vero exercitationem dolor numquam unde officiis
+          maiores totam vel iure neque laborum, harum reiciendis tempore saepe,
+          aliquam odit libero voluptas corporis amet rerum.
+        </p>
+      </main>
+    </div>
+  );
 };
 
 export default slug;
