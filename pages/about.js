c89f02b81b0240a10d7a0c549cddfeb0705f1d76
ManjiCoder <manjicoder@gmail.com>
2023-06-07 13:15:15 +0530

    Tutorial #27 Designing the About Page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,47 @@
 import React from "react";
+import styles from "../styles/About.module.css";
 
 const About = () => {
-  return <div>This is About page</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        reiciendis totam ipsum aut? Pariatur et aliquam ad, at, doloremque
+        eligendi fuga dignissimos sit quidem qui ducimus consectetur fugiat odit
+        nesciunt nihil excepturi libero? Magni, recusandae deserunt! Eaque
+        quisquam velit culpa incidunt porro? Debitis delectus ducimus veniam
+        exercitationem sequi? Placeat, quod quis maxime provident ut illo.
+        Ipsam.
+      </p>
+
+      <h2>Services Offered</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint autem
+        deleniti recusandae repellendus consectetur eligendi illum ut numquam
+        voluptatem obcaecati! Tempore, doloremque asperiores ea aliquid maxime
+        eligendi saepe quisquam cumque laboriosam voluptas dolorum odio!
+      </p>
+      <p>We offer the following services</p>
+      <ul>
+        <li>Service-1</li>
+        <li>Service-1</li>
+        <li>Service-1</li>
+        <li>Service-1</li>
+        <li>Service-1</li>
+        <li>Service-1</li>
+        <li>Service-1</li>
+      </ul>
+
+      <h2>Contact Us</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+        exercitationem distinctio quibusdam, eligendi libero dignissimos
+        voluptas assumenda cupiditate ad, ex, impedit veniam sapiente facere?
+      </p>
+    </div>
+  );
 };
 
 export default About;
